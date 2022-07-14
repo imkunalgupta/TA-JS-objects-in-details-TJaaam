ee4d4aa81e5fd7089721c7d2fcb5815311c52bbd
Kunal <mailto.guptakunal@gmail.com>
2022-07-14 14:03:18 +0530

    finish

---
+++ block-BJaahy/code/index.js
@@ -0,0 +1,12 @@
+class Square  {
+    constructor(side){
+    this.width = width;
+    this.height = height;
+    }
+    discription(){
+        alert(`The square is ${width} x ${height}`);
+    }
+    calcArea(){
+        return discription();
+    }
+};
