0782524ea7b821c7cb10d597e7f71b49cbbc21ff
Kunal <mailto.guptakunal@gmail.com>
2022-07-21 01:50:14 +0530

    finish

---
+++ block-BJaaim/code/data.js
@@ -0,0 +1,102 @@
+const quizCollection = [
+    {
+        title: 'What does HTML stand for?',
+        options: [
+            'Hypertext Machine Language',
+            'Hypertext and links markup language',
+            'Hypertext Markup Language',
+            'Hightext Machine Language',
+        ],
+        answerIndex: 2,
+    },
+    {
+        title: 'How is document type initialized in HTML5.?',
+        options: [
+            '</DOCTYPE HTML>',
+            '</DOCTYPE>',
+            '<!DOCTYPE HTML>',
+            '</DOCTYPE html>',
+        ],
+        answerIndex: 2,
+    },
+    {
+        title: 'Which of the following HTML Elements is used for making any text bold?',
+        options: [
+            '<p>',
+            '<i>',
+            '<li>',
+            '<b>',
+        ],
+        answerIndex: 3,
+    },
+    {
+        title: 'Which of the following characters indicate closing of a tag?',
+        options: [
+            '.',
+            '/',
+            '//',
+            '!',
+        ],
+        answerIndex: 1,
+    },
+    {
+        title: 'How many heading tags are there in HTML5?',
+        options: [
+            '2',
+            '3',
+            '5',
+            '6',
+        ],
+        answerIndex: 3,
+    },
+    {
+        title: 'Which of the following is a disadvantage of using JavaScript?',
+        options: [
+            'Client-side JavaScript does not allow the reading or writing of files.',
+            'JavaScript can not be used for Networking applications because there is no such support available.',
+            'JavaScript does not have any multithreading or multiprocess capabilities.',
+            'All of the above.',
+        ],
+        answerIndex: 3,
+    },
+    {
+        title: 'How can you get the total number of arguments passed to a function?',
+        options: [
+            'Using args.length property',
+            'Using arguments.length property',
+            'Both of the above.',
+            'None of the above.',
+        ],
+        answerIndex: 1,
+    },
+    {
+        title: 'Which built-in method returns the calling string value converted to upper case?',
+        options: [
+            'toUpperCase()',
+            'toUpper()',
+            'changeCase(case)',
+            'None of the above.',
+        ],
+        answerIndex: 0,
+    },
+    {
+        title: 'Which of the following function of String object executes the search for a match between a regular expression and a specified string?',
+        options: [
+            'concat()',
+            'match()',
+            'replace()',
+            'search()',
+        ],
+        answerIndex: 3,
+    },
+    {
+        title: 'Which of the following function of Array object removes the last element from an array and returns that element?',
+        options: [
+            'pop()',
+            'push()',
+            'join()',
+            'map()',
+        ],
+        answerIndex: 0,
+    },
+]
